382dc81f9a6099076cde37ae683b1a67e0209a3f
mascip (Pierre Masci) <piemas25+git@gmail.com>
2013-11-07 08:22:51 +0000

    added uri_base, to run from non-root directory

--- lib/HabitLab.pm
+++ lib/HabitLab.pm
@@ -5,14 +5,22 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
+
+# Pass values from config to templates
 hook 'before_template' => sub { 
     my $request = shift; 
 
-    # Pass values from config to templates
     $request->{js_files} =  config->{js_files};
     $request->{css_files} =  config->{css_files};
 };
 
+# Base folder (if the website is not running under '/' )
+hook before_template_render => sub {
+    my $tokens = shift;
+    $tokens->{uri_base} = request->base->path;
+};
+
+### ROUTES
 get '/' => sub {
     template 'index2';
 };
